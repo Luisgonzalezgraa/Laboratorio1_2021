30436f6d5eb0f5569cb4f974c6a033e7efc5ec97
Luis Gonzalez <luis.gonzalez.gr@usach.cl>
2021-04-18 18:11:29 -0400

    Se agregaron funciones que permiten desencripatar el ultimo elemento de la lista social Network, se espera a preguntar al profesor si se puede realizar esto.

--- socialNetwork.rkt
+++ socialNetwork.rkt
@@ -1,8 +1,9 @@
 #lang racket
 (provide (all-defined-out))
 
+
 (define (socialNetwork nameRedSocial date contenido1 contenido2)
-  (list nameRedSocial date contenido1 contenido2)
+  (list nameRedSocial date contenido1 (decryp contenido2))
   )
 
 ;Selector Nombre:
@@ -32,3 +33,26 @@
 (define (ingresarDecryp contenido2)
   (cadddr contenido2)
   )
+
+
+
+(define encryptFn (lambda (s) (list (list->string (reverse (string->list s))))))
+
+
+(define (decryp lista)
+  (if (null? (cdr lista))
+      (encryptFn (car lista))
+      (concatenar (encryptFn (car lista))(decryp (cdr lista)))
+      )
+     
+  )
+
+(define (concatenar lista1 lista2)
+  (if(null? lista1)
+     lista2
+     (cons (car lista1) (concatenar (cdr lista1) lista2))
+  )
+)
+
+
+
