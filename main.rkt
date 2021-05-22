5f8d999c3284d135334ece7465d8b4e047ac842a
Luis <luis.gonzalez.gr@usach.cl>
2021-05-21 22:46:08 -0400

    Se arreglo la funcion post, ademas se agregaron nuevas funciones y se agrego la funcion share dejandola lista.

--- main.rkt
+++ main.rkt
@@ -4,6 +4,7 @@
 (require "socialRegister.rkt")
 (require "usuarioActivo.rkt")
 (require "date.rkt")
+(require "posteo.rkt")
 
 ;Funcion register: 
 ;Dom: socialNetworw X string X string
@@ -74,6 +75,7 @@
                 (if(null? socialNet)
                    (display "Nombre o Contraseña incorrectas.")
                    (if(comparardoslistas socialNet (getUsuarios socialNet) users)
+                      (if(null? (getPosteo socialNet))
                     (socialNetwork
                      (getNombre socialNet)
                      (getFecha socialNet)
@@ -81,10 +83,22 @@
                      (getDecryp socialNet)
                      (getUsuarios socialNet)
                      (getUsuarioActivo socialNet)
-                     (concatenar (getPosteo socialNet)(list(list (cadr(getUsuarioActivo socialNet)) (map(getEncryp socialNet) date) ((getEncryp socialNet) content) (map(getEncryp socialNet) users))))
+                     (concatenar (getPosteo socialNet)(list(posteo +1 (cadr(getUsuarioActivo socialNet)) (map(getEncryp socialNet) date) ((getEncryp socialNet) content) (map(getEncryp socialNet) users))))
                      (getFollow socialNet)
                      (getShare socialNet)
                      )
+                    (socialNetwork
+                     (getNombre socialNet)
+                     (getFecha socialNet)
+                     (getEncryp socialNet)
+                     (getDecryp socialNet)
+                     (getUsuarios socialNet)
+                     (getUsuarioActivo socialNet)
+                     (concatenar (getPosteo socialNet)(list(posteo (+ (car (last (getPosteo socialNet)))1) (cadr(getUsuarioActivo socialNet)) (map(getEncryp socialNet) date) ((getEncryp socialNet) content) (map(getEncryp socialNet) users) )))
+                     (getFollow socialNet)
+                     (getShare socialNet)
+                     ))
+                    
                      (display "Al usuario dirigido el post no existe")
               )))))
   )
@@ -92,7 +106,7 @@
 ;Dom: socialNetworw X string X string
 ;Rec: socialNetworw
 ;Recursion: Natural
-;Ejemplo de uso:(define eFB (((login FB "usuario1" "pass1" post)(date 21 3 2020))"mi primer post" "user1"))
+;Ejemplo de uso:
 
 (define follow(lambda(socialNet) (lambda(date)
               (lambda (user)
@@ -115,6 +129,39 @@
               ))))))
   )
 
+;Funcion : 
+;Dom: socialNetworw X string X string
+;Rec: socialNetworw
+;Recursion: Natural
+;Ejemplo de uso:
+
+(define share(lambda(socialNet) (lambda(date)
+               
+              (lambda (postId . users)
+                (if(null? socialNet)
+                   (display "Nombre o Contraseña incorrectas.")
+                   (if(comparardoslistas socialNet (getUsuarios socialNet) users)
+                      (if(estaid? (getPosteo socialNet) postId)
+                    (socialNetwork
+                     (getNombre socialNet)
+                     (getFecha socialNet)
+                     (getEncryp socialNet)
+                     (getDecryp socialNet)
+                     (getUsuarios socialNet)
+                     (getUsuarioActivo socialNet)
+                     (getPosteo socialNet)
+                     (getFollow socialNet)
+                     (concatenar(getShare socialNet)(list (list (cadr(getUsuarioActivo socialNet)) (getId (obtenerposicion (getPosteo socialNet) postId)) (getContent (obtenerposicion (getPosteo socialNet) postId)) (map(getEncryp socialNet) users))))
+                     )
+                    (display "El id del post no existe")
+                    )
+                     (display "Al usuario dirigido el post no existe")
+              )))))
+  )
+
+
+
+
 
 
 
@@ -132,22 +179,22 @@
 )
 
 ;-------------------------------------
-;Funcion estaenlista?: Ve si un elemento es parte de la lista
+;Funcion estaid?: Funcion auxiliar que sirve para ver si el post con el numero existe dentro de la plataforma
 ;Dom: lista x (string-int-entre otros)
 ;Rec: Booleano
 ;Recursion: Cola
 
-(define (estaenlista? lista parametro)
+(define (estaid? lista parametro)
   (if (null? lista)
       #f
-      (if (equal? (car lista) parametro)
+      (if (equal? (getId (car  lista)) parametro)
           #t
-          (estaenlista? (cdr lista) parametro)
+          (estaid? (cdr lista) parametro)
           )
       )
   )
 
-;Funcion filtradoPorUsuarios: filtra una lista de listas para obtener una lista con los usuarios registrados en la plataforma
+;Funcion estaenlistadelistas?: funcion que sirve para saber si un usuario existe dentro de una lista de listas
 ;Dom: Lista x lista
 ;Rec: Lista
 ;Recursion: Natural
@@ -160,7 +207,7 @@
           )
       )
   )
-;Funcion filtradoPorUsuarios: filtra una lista de listas para obtener una lista con los usuarios registrados en la plataforma
+;Funcion comparardoslistas: filtra una lista de listas para obtener una lista con los usuarios registrados en la plataforma
 ;Dom: Lista x lista
 ;Rec: Lista
 ;Recursion: Natural
@@ -176,7 +223,7 @@
          )
       )
   )
-;Funcion filtradoPorUsuarios: filtra una lista de listas para obtener una lista con los usuarios registrados en la plataforma
+;Funcion estanlosparametros? filtra una lista de listas para obtener una lista con los usuarios registrados en la plataforma
 ;Dom: Lista x lista
 ;Rec: Lista
 ;Recursion: Natural
@@ -189,28 +236,54 @@
       )
    )
  )
-;Funcion filtradoPorUsuarios: filtra una lista de listas para obtener una lista con los usuarios registrados en la plataforma
+;Funcion obtenerposicion filtra una lista de listas para obtener una lista con los usuarios registrados en la plataforma
 ;Dom: Lista x lista
 ;Rec: Lista
 ;Recursion: Natural
-(define (obtenerposicion lista parametro parametro2)
+(define (obtenerposicion lista parametro)
   (if (null? lista)
       #f
-      (if (and (equal? (getName (car  lista)) parametro)(equal? (getPassword (car  lista)) parametro2))
+      (if (equal? (getId (car  lista)) parametro)
            (car lista)
-          (obtenerposicion (cdr lista) parametro parametro2)
+          (obtenerposicion (cdr lista) parametro)
       )
    )
  )
 
-;Funcion : 
-;Dom: socialNetworw X string X string
-;Rec: socialNetworw
+;Funcion encryptFn : Funcion que sirve para encryptar informacion
+;Dom: string
+;Rec: string
 ;Recursion: Natural
 ;Ejemplo de uso:
-
 (define encryptFn (lambda (s) (list->string (reverse (string->list s)))))
 
 
-
+;Funcion decryptFn : Funcion que sirve para decencryptar informacion
+;Dom: string
+;Rec: string
+;Recursion: Natural
+;Ejemplo de uso:
 (define decryptFn(lambda(s) encryptFn (encryptFn s)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(define emptyFB (socialNetwork "fb" (date 25 10 2021) encryptFn decryptFn null null null null null))
+(define FB (register (register (register emptyFB (date 25 10 2021) "user1" "pass1") (date 25 10 2021) "user2" "pass2") (date 25 10 2021) "user3" "pass3"))
+(define FB2 (((login FB "user1" "pass1" post) (date 28 10 2021)) "10nd post" "user2" "user3"))
+(define FB3 (((login FB2 "user2" "pass2" post) (date 28 10 2021)) "10nd post" "user3"))
+(define FB4 (((login FB3 "user3" "pass3" post) (date 28 10 2021)) "10nd post" "user1"))
+(define FB5 (((login FB4 "user3" "pass3" follow) (date 28 10 2021))"user1"))
+(define FB6 (((login FB5 "user3" "pass3" share) (date 28 10 2021)) 3 "user1"))
+(define FB7 (((login FB6 "user1" "pass1" share) (date 28 10 2021)) 2 "user2" "user3"))
